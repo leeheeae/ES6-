673eed881bcb63006768f7b7cd7dfea10530880d
Heeae <chromeee@daum.net>
2023-06-28 22:15:26 +0900

    지연성을 가지는 값에 느긋한 range 사용 시 속도 비교

--- L.range.js
+++ L.range.js
@@ -43,3 +43,24 @@ function test(name, time, f) {
  */
 test('range', 10, () => reduce(add, range(10000000)));
 test('L.range', 10, () => reduce(add, L.range(10000000)));
+
+// # take
+// 지연성을 가지는 값을 이터레이터로 만들게 되면 모두 조합을 할 수 잇음
+const take = (l, iter) => {
+  let res = [];
+  for (const a of iter) {
+    res.push(a);
+    if (res.length == l) return res;
+  }
+
+  return res;
+};
+
+// 배열에 들어있는 1,2번째 값을 꺼내서 연산을 하거나, 몇 개의 length가 될 지 모르는 값을 꺼내서 연산을 하는 작업일 경우에는 느긋한 레인지로 작업을 하면 속도면에서 효율을 얻을 수 있음
+console.time('');
+console.log(take(5, range(10000000))); // : 76.362ms
+console.timeEnd('');
+
+console.time('');
+console.log(take(5, L.range(10000000))); //: 0.046ms
+console.timeEnd('');
